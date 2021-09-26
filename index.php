fe4d987293b362df00e2a7ff687de08ac4e8a8c0
gulserliler18 <43846664+gulserliler18@users.noreply.github.com>
2021-09-26 19:47:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     echo "<a href=$url  class='na'>Calendar</a>";
     $url = str_replace("calendar","signup",$url);
     echo "<a href=$url  class='na'>Sign Up</a>";
-    echo "<button onclick='darkmode()'>Toggle dark mode</button>";
+    echo "<a onclick='darkmode()'>Toggle dark mode</button>";
 
 ?>
 </nav>
